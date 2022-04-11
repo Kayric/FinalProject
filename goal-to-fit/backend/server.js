e18859c53bdcaf7a7250bf85c6238971bb62a3cb
Kayric <98443692+Kayric@users.noreply.github.com>
2022-04-11 22:49:03 +0700

    change port to 4001

--- goal-to-fit/backend/server.js
+++ goal-to-fit/backend/server.js
@@ -1,36 +1,57 @@
 const express = require("express");
-const cors = require("cors");
+const bodyParser = require("body-parser");
 const mongoose = require("mongoose");
+const cors = require("cors");
 
+const PORT = process.env.PORT || 4001;
 require("dotenv").config();
 
-const app = express();
-const port = process.env.PORT || 5000;
+const exercisesRouter = require("./routes/exercises");
+const usersRouter = require("./routes/users");
 
-const bodyParser = require("body-parser");
+const app = express();
+
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 
-app.use(cors());
+app.use(
+  cors({
+    origin: "*",
+    optionsSuccessStatus: 200, // some legacy browsers (IE11, various SmartTVs) choke on 204
+  })
+);
 app.use(express.json());
 
-const uri = process.env.ATLAS_URI;
-mongoose.connect(uri, {
-  useNewUrlParser: true,
-  useCreateIndex: true,
-  useUnifiedTopology: true,
-});
 const connection = mongoose.connection;
 connection.once("open", () => {
   console.log("MongoDB database connection established successfully");
 });
 
-const exercisesRouter = require("./routes/exercises");
-const usersRouter = require("./routes/users");
-
 app.use("/exercises", exercisesRouter);
 app.use("/users", usersRouter);
 
-app.listen(port, () => {
-  console.log(`Server is running on port: ${port}`);
-});
+const uri = process.env.ATLAS_URI;
+// mongoose.connect(uri, {
+//   useNewUrlParser: true,
+//   useCreateIndex: true,
+//   useUnifiedTopology: true,
+// });
+
+const boot = async () => {
+  // Connect to mongodb
+  await mongoose.connect(uri, {
+    useNewUrlParser: true,
+    useCreateIndex: true,
+    useUnifiedTopology: true,
+  });
+  // Start express server
+  app.listen(PORT, () => {
+    console.log(`Server is listening on port ${PORT}`);
+  });
+};
+
+boot();
+
+// app.listen(port, () => {
+//   console.log(`Server is running on port: ${port}`);
+// });
