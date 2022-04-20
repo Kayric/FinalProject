f067616232819a95e17deef0c820d5e9754dc528
Kayric <98443692+Kayric@users.noreply.github.com>
2022-04-20 16:14:17 +0700

    specific duration in homepage

--- goal-to-fit/src/components/exercises-list.js
+++ goal-to-fit/src/components/exercises-list.js
@@ -73,7 +73,7 @@ export default class ExercisesList extends Component {
             <tr>
               <th>Username</th>
               <th>Description</th>
-              <th>Duration</th>
+              <th>Duration (in minutes)</th>
               <th>Date</th>
               <th>Actions</th>
             </tr>
